127c079111357cfb8d3898d74703e10e59f2c7ef
Nick Oki <nick.oki@live.com>
2016-09-16 13:35:21 -0400

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,30 @@
+/* script.js */
+
+// TODO Set up html & css
+
+// TODO Create card object
+
+// TODO make card constructor
+
+// TODO set up hard-coded card content
+
+// TODO Write card flip function
+
+// TODO Write next card function
+
+// TODO Scoreboard function
+
+
+
+
+
+
+// TODO SUBMISSION
+/*
+Deploy online
+
+*/
+
+// TODO
+// TODO Write your own cards
+// TODO Card animations
